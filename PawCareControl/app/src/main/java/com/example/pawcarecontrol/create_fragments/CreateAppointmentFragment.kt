009d7ff1559ea85d9060c143ffad24cbab872134
Jeffrey <mixjeff@hotmail.com>
2024-05-12 13:03:48 -0600

    Creacion de citas

--- PawCareControl/app/src/main/java/com/example/pawcarecontrol/create_fragments/CreateAppointmentFragment.kt
+++ PawCareControl/app/src/main/java/com/example/pawcarecontrol/create_fragments/CreateAppointmentFragment.kt
@@ -1,11 +1,13 @@
 package com.example.pawcarecontrol.create_fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.pawcarecontrol.R
 
@@ -15,13 +17,22 @@ class CreateAppointmentFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_create_appointment, container, false)
-
         val btnAddAppointment = root.findViewById<Button>(R.id.btnAddAppointment)
+        val mascots = arrayOf("Perro", "Gato", "Pájaro", "Pez", "Conejo")
+        val doctors = arrayOf("Pedro Parker", "Natalia Jimenez", "Brandon Alexis", "Alexa Quintanilla", "Alejandro Gomez")
 
-        btnAddAppointment.setOnClickListener{
+        val autoCompleteTextViewPet = root.findViewById<AutoCompleteTextView>(R.id.mascota_input)
+        val autoCompleteTextViewDoc = root.findViewById<AutoCompleteTextView>(R.id.doctor_input)
+        val adapterPet = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, mascots)
+        autoCompleteTextViewPet.setAdapter(adapterPet)
+        val adapterDoc = ArrayAdapter(requireContext(), android.R.layout.simple_dropdown_item_1line, doctors)
+        autoCompleteTextViewDoc.setAdapter(adapterDoc)
+
+        btnAddAppointment.setOnClickListener {
             findNavController().navigate(R.id.action_createAppointmentFragment_to_listAppointmentsFragment)
         }
 
         return root
     }
+
 }
