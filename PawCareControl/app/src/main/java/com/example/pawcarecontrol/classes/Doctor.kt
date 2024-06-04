259bbbe336cb94ce2a4f0d8b4bae5dd8a8047602
Karletty <karletty.carolina@gmail.com>
2024-06-04 11:20:45 -0600

    iniciando a configurar el adaptador para listar doctores

---
+++ PawCareControl/app/src/main/java/com/example/pawcarecontrol/classes/Doctor.kt
@@ -0,0 +1,9 @@
+package com.example.pawcarecontrol.classes
+
+data class Doctor (
+    val name: String,
+    val phone: String,
+    val gender: String,
+    val dui: String,
+    val birthday: String,
+)
